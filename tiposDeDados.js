7a6edbda6d0fde88639688108dd418bcb0b61440
Samuel Mazzucato <89219911+Scmazzucato@users.noreply.github.com>
2021-11-01 15:44:16 -0300

    inicio javascript

---
+++ tiposDeDados.js
@@ -0,0 +1,24 @@
+const inteiro = 10;
+const decimal =10.5;
+const bool = false;
+const nome = "Samuel";
+let copo = null;
+let televisão;
+let pessoa = {
+    nome:"Samuel"
+}
+const symbol = Symbol()
+
+console.log("Tipo da variavel inteiro", typeof inteiro);
+console.log("Tipo da variavel decimal",typeof decimal);
+console.log("Tipo da variavel bool",typeof bool);
+console.log("Tipo da variavel nome",typeof nome);
+console.log("Tipo da variavel copo",typeof copo);
+console.log("Tipo da variavel televisão",typeof televisão);
+console.log("Tipo da variavel pessoa",typeof pessoa);
+console.log("Tipo da variavel symbol",typeof symbol);
+
+
+
+
+
---
+++ variaveisConstantes.js
@@ -0,0 +1,4 @@
+var cor = "branca";
+let nome = "Samuel";
+const empresa = "Devaria";
+console.log(cor, nome, empresa);
